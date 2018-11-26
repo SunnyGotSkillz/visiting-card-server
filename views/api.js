1f8dc2d30ef851b9f1bb1f2a8ed7599c41309d23
SunnyGotSkillz <sunnyvinay7@gmail.com>
2018-11-25 19:08:42 -0800

    Added a POST method to the API

--- views/api.js
+++ views/api.js
@@ -6,4 +6,28 @@ router.get('/', (req, res) => {
   res.send('Hello World from API');
 });
 
+router.get('/visitingcard', (req, res) => {
+  var obj = '{'
+       +'"name" : "Sunny",'
+       +'"email"  : "sunnyvinay7@gmail.com",'
+       +'"phone" : "408-656-7734"'
+       +'}';
+  console.log(obj);
+  res.send(obj);
+});
+
+router.post('/visitingcard', (req, res) => {
+	var jsonString = '';
+
+    req.on('data', function (data) {
+       jsonString += data;
+    });
+
+    req.on('end', function () {
+       console.log(JSON.parse(jsonString));
+    });
+  res.send('SUCCESS');
+});
+
+
 module.exports = router;
